1e76fa7117702c30efc4f172dc42bd0e5ff493e3
Vignesh V Pai <vigneshvpai@gmail.com>
2021-02-23 11:31:52 +0530

    Added splashscreen in android

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import RNBootSplash from 'react-native-bootsplash';
+
 import RootNavigation from './src/navigation';
 
 const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide({fade: true});
+  }, []);
+
   return (
     <SafeAreaProvider>
       <RootNavigation />
