54ccc49e04610530a641784f257625c4d67eeea9
Vignesh V Pai <vigneshvpai@gmail.com>
2021-02-23 11:34:51 +0530

    Removed API interceptor and logs

--- src/services/api.js
+++ src/services/api.js
@@ -4,33 +4,4 @@ const api = axios.create({
   baseURL: 'https://api.openbrewerydb.org',
 });
 
-api.interceptors.request.use(
-  async (req) => {
-    console.log('Request: ', req);
-    return req;
-  },
-  (config) => {
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
-
-api.interceptors.response.use(
-  (response) => {
-    __DEV__ && console.log('Response: ', response);
-    return response;
-  },
-  (error) => {
-    console.log(error);
-    if (error.response) {
-      console.log('error.response: ', error.response);
-    } else {
-      console.log('error.config: ', error.config);
-    }
-    return Promise.reject(error);
-  },
-);
-
 export default api;
